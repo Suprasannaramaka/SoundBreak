dfc83d6624e3b7c8b3934725fc82a643e3317cd3
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-28 21:21:25 +0530

    i

--- api/App.tsx
+++ api/App.tsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from 'react'
-import { supabase } from '../utils/supabase.ts'
+import supabase from '../utils/supabase';
 
 function Page() {
   const [todos, setTodos] = useState([])
