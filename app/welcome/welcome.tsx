2ee6d8047b7cd76d7442d30eb4ece1e5bda33aac
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-29 14:43:55 +0530

    i

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,12 +1,10 @@
-import logo.avif from "../../public/logo.avif";
-
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[125] max-w-[100vw] p-4">
-           <img src="" alt="" />
+           <img src="../../public/logo.png" alt="" />
           </div>
         </header>
         <div className="max-w-[300] w-full space-y-6 px-4">
