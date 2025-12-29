e10e1f01f5e7b33070b2c5eb6afaad0d3b37cc41
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-29 18:30:27 +0530

    i

--- src/auth.tsx
+++ src/auth.tsx
@@ -20,9 +20,7 @@ export default function Auth() {
         </div>
       </div>
       <div className="bg-muted relative hidden lg:block">
-        <img
-          src="public/coverimage.jpg"
-          alt="Image"
+        <img src="/coverimage.jpg" alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
