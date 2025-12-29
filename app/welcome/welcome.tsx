25f47b78bd318e7f633bd8106d3536447d9f3c20
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-29 15:25:25 +0530

    i

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,20 +1,17 @@
 import auth from "../../src/auth";
 export function Welcome() {
   return (
+    <div>
     <main className="flex-col">
       <div className="">
         <header className="bg-stone-400 align-top items-stretch">
            <img src="/logo.png" alt="" className="min-w-5 h-20 items-baseline "/>
-           <ol className="align-start">
-            <li>searchbar</li>
-               <li>Premium</li>
-                  <li>Download</li>
-           </ol>
-           <ol>
-            <li>Sign-Up</li>
-            <li>LogIn</li>
-           </ol>
         </header>
         </div>
        </main>
+       <div className="">
+       </div>
+       <footer className="">
+       </footer>
+       </div>
        )}
