f6e315d6d1bcb20f9d080643f229dd9d4ef754ad
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-29 14:46:45 +0530

    i

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,7 +4,7 @@ export function Welcome() {
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[125] max-w-[100vw] p-4">
-           <img src="../../public/logo.png" alt="" />
+           <img src="/logo.png" alt="" />
           </div>
         </header>
         <div className="max-w-[300] w-full space-y-6 px-4">
