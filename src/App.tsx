eb6da3478e57bb268a68ecaa31596303fe1a04ad
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-29 22:57:20 +0530

    i

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { LoginForm } from "./components/pages/loginform"
-import { SignupForm } from "./components/pages/signup-form"
+import { Routes, Route } from "react-router-dom";
+import { LoginForm } from "./components/pages/loginform";
+import { SignupForm } from "./components/pages/signup-form";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
       </Routes>
-    </BrowserRouter>
   )
 }
 
