2899069d6f6801aa2dfd127e483ecda1474a2d05
Ramaka Suprasanna <ramakasuprasanna6@gmail.com>
2025-12-29 23:51:40 +0530

    i

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import { Routes, Route } from "react-router-dom";
 import  LoginForm from "./components/pages/loginform";
 import  SignupForm  from "./components/pages/signup-form";
@@ -6,8 +5,8 @@ import  SignupForm  from "./components/pages/signup-form";
 function App() {
   return (
       <Routes>
-        <Route path="/loginForm" element={<LoginForm />} />
-        <Route path="/signupForm" element={<SignupForm />} />
+        <Route path="./loginForm" element={<LoginForm />} />
+        <Route path="./signupForm" element={<SignupForm />} />
       </Routes>
   );
 }
